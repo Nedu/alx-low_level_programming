e89ef729fca227a37f7d7f710299dc58c0c825a1
Nedu Robert <nedumrobert@gmail.com>
2022-10-13 19:35:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * *main - C program that prints a string using puts.
- * *
- * *Return:returns 0
- * */
+ * main - hello, puts
+ *
+ * Return: Returns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
