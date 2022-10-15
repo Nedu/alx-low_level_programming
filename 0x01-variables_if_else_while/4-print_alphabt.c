ae5f69f85c72127bbcb31048ca6fce3e4bdca3d8
Nedu <nedumrobert@gmail.com>
2022-10-14 20:26:49 -0400

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print_alphabt
+ *Return: Returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
