a5fcf06e10214eace116955823ad3f89155c146f
Nedu <nedumrobert@gmail.com>
2022-10-18 16:04:24 -0400

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,8 @@
  *
  * @c: input character to check for alphabetic character
  *
- * Return: Returns 1 if c is a letter, lowercase or uppercase, otherwise return 0
+ * Return: Returns 1 if c is a letter, lowercase or uppercase, 
+ * otherwise return 0
  */
 int _isalpha(int c)
 {
