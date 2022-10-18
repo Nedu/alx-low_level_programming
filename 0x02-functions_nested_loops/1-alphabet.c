acb0ee4f2fc405fef6e0548a77f0435c2164109f
Nedu <nedumrobert@gmail.com>
2022-10-18 11:42:48 -0400

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Prints the alphabet, in lowercase, followed by a new line.
+ * Return: Returns 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Prints 10 times the alphabet, in lowercase, followed by a new line.
+ * Return: Returns 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i, k;
+
+	for (k = 0; k < 10; k++)
+	{
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
