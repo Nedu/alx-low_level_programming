ed8e14e85a07a27712a1c1869d1b16868d0e6e93
Nedu <nedumrobert@gmail.com>
2022-10-18 17:01:36 -0400

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
  *
  * @n:Number whose sign is to be checked
  *
- * Return: Retutns 1 if n is greater than 0; 0 if n equals 0; 
+ * Return: Returns 1 if n is greater than 0;
+ * 0 if n equals 0;
  * -1 if n is less than 0
  */
 int print_sign(int n)
