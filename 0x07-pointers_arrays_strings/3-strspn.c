8aef2808c06fa32092ab415994505d6364e18221
Nedu Robert <nedumrobert@gmail.com>
2022-10-31 22:09:39 -0400

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,8 @@
  * _strspn - function that gets the length of a prefix substring
  * @s: string
  * @accept: string
- * Return: Returns the number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: Returns the number of bytes in the initial segment of s 
+ * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
