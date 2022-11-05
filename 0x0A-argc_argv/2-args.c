2e8153aa2923ff9b88391fc0e9232f5d9686f452
Nedu Robert <nedumrobert@gmail.com>
2022-11-04 22:04:30 -0400

    C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives
+ * @argc: int input
+ * @argv: char input
+ * Return: Returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int counter;
+
+	for (counter = 0; counter < argc; counter++)
+		printf("%s\n", argv[counter]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @argc: int input
+ * @argv: char input
+ * Return: Returns 1 or 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
