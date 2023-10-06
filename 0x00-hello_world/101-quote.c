b794005054ffd6e65d0d3a2b7ff103deaaece808
Nedu Robert <nedumrobert@gmail.com>
2023-10-05 23:17:30 -0400

    additonal tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - UNIX is basically a simple
+ * operating system, but you have to be a genius
+ * to understand the simplicity
+ * Author - Nedu Robert
+ *
+ * Return: Returns 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
