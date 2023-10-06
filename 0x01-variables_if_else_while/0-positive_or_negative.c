09bc7f75b8197f45534d01427978926a663d4142
Nedu Robert <nedumrobert@gmail.com>
2023-10-06 17:58:39 -0400

    updated task 0 to use betty standards

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,16 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Positive anything is better than negative nothing
+ * This program will assign a random number to the variable n
+ * each time it is executed. Complete the source code in order
+ * to print whether the number stored in the variable n is
+ * positive or negative.
+ * Author - Nedu Robert
+ *
+ * Return: Returns 0
+ */
 int main(void)
 {
 	int n;
@@ -11,14 +21,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0) {
+	if (n > 0) 
 		printf("%d is positive\n", n);
-	}
-	if (n == 0) {
+	if (n == 0) 
 		printf("%d is zero\n", n);
-	}
-	if (n < 0) {
+	if (n < 0) 
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
