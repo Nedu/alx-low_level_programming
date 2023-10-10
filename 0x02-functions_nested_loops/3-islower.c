fe4753acfcf5fa9f01f02b186d4958aeaa06aa8a
Nedu Robert <nedumrobert@gmail.com>
2023-10-10 17:06:44 -0400

    fixed betty issues

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,15 @@
 /**
  * _islower - islower
  * Function that checks for lowercase character.
+ * @c: the character to be checked.
  * Author - Nedu Robert
  *
- * Return: Returns void
+ * Return: Returns 1 for lowercase character, 0 otherwise.
  */
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 97 and c <= 122) {
         return (1);
     }
     return (0);
-
 }
-
