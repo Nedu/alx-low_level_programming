1085a91d994aa16a3c7dc9627abff197cc284483
Nedu Robert <nedumrobert@gmail.com>
2023-10-10 17:19:50 -0400

    Sign task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,23 +6,24 @@
  * @n: the number to be checked.
  * Author - Nedu Robert
  *
- * Return: Returns 1 for lowercase character, 0 otherwise.
+ * Return: Returns 1 for positive number,
+ * -1 for negative number, or 0 for zero number.
  */
 int print_sign(int n)
 {
 	if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
