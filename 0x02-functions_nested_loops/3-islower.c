fa1a015475f6fa6e36bc4c450e66e099081d12f1
Nedu Robert <nedumrobert@gmail.com>
2023-10-10 17:03:57 -0400

    isLower task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - islower
+ * Function that checks for lowercase character.
+ * Author - Nedu Robert
+ *
+ * Return: Returns void
+ */
+int _islower(int c);
+{
+	if (c >= 97 and c <= 122) {
+        return (1);
+    }
+    return (0);
+
+}
+
