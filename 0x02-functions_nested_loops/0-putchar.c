e3cdac8b7b793f8a07eb2b66301ecf9140910e55
Nedu Robert <nedumrobert@gmail.com>
2023-10-10 16:16:07 -0400

    Functions, nested loops task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - _putchar
+ * program that prints _putchar, followed by a new line.
+ * Author - Nedu Robert
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	_putchar("_putchar\n")
+	return (0);
+}
