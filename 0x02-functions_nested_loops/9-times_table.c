c8dc9f85f245c8661153528ab9fc5942dd7fc4fd
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 19:08:06 -0400

    fixed duplicate column

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,9 @@ void times_table(void)
 
     for (i = 0; i <= 9; i++)
     {
-        _putchar('0');
         for (j = 0; j <= 9; j++)
         {
             result = i * j;
-            _putchar(',');
-            _putchar(' ');
             if (result <= 9)
             {
                 _putchar(' ');
