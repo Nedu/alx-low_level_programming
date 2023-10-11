9fd9e4fe0299a21f9a6039c480fec65fd26f147e
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 18:54:13 -0400

    times_table task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Learn your times table
+ * Function that prints the 9 times table, starting with 0.
+ * Author - Nedu Robert
+ *
+ * Return: Returns void
+ */
+void times_table(void);
+{
+	int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar('0');
+        for (j = 0; j <= 9; j+=)
+        {
+            result = i * j;
+            _putchar(',');
+            _putchar(' ');
+            if (result <= 9)
+            {
+                _putchar(' ');
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
+}
