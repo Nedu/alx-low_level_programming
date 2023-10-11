323735fdbcc47131398a1537e8522d305e8020b3
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 18:03:55 -0400

    print_last_digit task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - There are only 3 colors, 10 digits,
+ * and 7 notes; it's what we do with them that's important
+ * Function that prints the last digit of a number.
+ * @c: input number.
+ * Author - Nedu Robert
+ *
+ * Return: Last digit of number
+ */
+int print_last_digit(int c)
+{
+	return c % 10;
+}
