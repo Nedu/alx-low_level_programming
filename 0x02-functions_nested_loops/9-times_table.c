90b46cc33992edfb6b31e9c7b42f8c3a162ef3dd
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 19:15:47 -0400

    fixed error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			_putchar(' ');
+			
 			if (result <= 9)
 			{
 				_putchar(' ');
@@ -24,6 +24,8 @@ void times_table(void)
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
