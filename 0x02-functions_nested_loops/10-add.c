fe9757181c9e8eeecee548d8b5410a2625cfc6e1
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 22:18:15 -0400

    add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - a + b
+ * Function that adds two integers and returns the result.
+ * @a: input number.
+ * @b: input number.
+ * Author - Nedu Robert
+ *
+ * Return: Returns sum of the two inputs
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
