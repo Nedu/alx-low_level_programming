9d386ad01a2d024da33049c9fcce4322133ce36a
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 23:09:56 -0400

    fixed issues

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,7 +38,8 @@ void print_times_table(int n)
 					_putchar(((result / 10) % 10) + '0');
 					_putchar((result % 10) + '0');
 				}
-				else {
+				else
+				{
 					if (j != 0) 
 					{
 						_putchar(',');
