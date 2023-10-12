e26f0a500051faf4fbbec45f09589accdcb1e5b2
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 22:53:07 -0400

    fixed task issue

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,6 +22,7 @@ void print_times_table(int n)
             {
                 result = i * j;
                 _putchar(result + '0');
+                _putchar(', ');
             }
             _putchar('\n');
         }
