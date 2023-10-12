5805960c56d9f6212f841f25e9b8ad455dd264d1
Nedu Robert <nedumrobert@gmail.com>
2023-10-11 22:49:17 -0400

    print_times_table task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - The World looks like a multiplication-table,
+ * or a mathematical equation, which, turn it how you will,
+ * balances itself
+ * Function that prints the n times table, starting with 0.
+ * @n: input number.
+ * Author - Nedu Robert
+ *
+ * Return: Returns void
+ */
+void print_times_table(int n)
+{
+    int i, result;
+
+	if (n <= 15 && n >= 0) {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                result = i * j;
+                _putchar(z + '0');
+            }
+        }
+    }
+}
