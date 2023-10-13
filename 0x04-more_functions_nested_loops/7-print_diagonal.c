8aaee27596d672cf1f07b48598e16c587e5f17c6
Nedu Robert <nedumrobert@gmail.com>
2023-10-13 13:47:55 -0400

    fixed betty issues

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,20 +16,20 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-            for (j = 0; j < n; j++)
-            {
-                if (j == i)
-                    _putchar('\\');
-                if (j < i)
-                {
-                    _putchar(' ');
-                }
-            }
-	        _putchar('\n');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
 	}
-    else
-    {
-        _putchar('\n');
-    }
+	else
+	{
+		_putchar('\n');
+	}
 }
