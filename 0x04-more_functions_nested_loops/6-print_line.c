bb835c159c407f7d8a4407a3edfc2ab7cf19d0fe
Nedu Robert <nedumrobert@gmail.com>
2023-10-13 13:35:43 -0400

    print_line task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - The shortest distance between two points is a straight line
+ * Function that draws a straight line in the terminal.
+ * @n - input number
+ * Author - Nedu Robert
+ *
+ * Return: void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
