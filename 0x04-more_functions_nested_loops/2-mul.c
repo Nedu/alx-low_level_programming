84fa578ae8d1a07f5a42bb7b0316ad6a99606b15
Nedu Robert <nedumrobert@gmail.com>
2023-10-12 22:57:47 -0400

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Collaboration is multiplication
+ * Function that multiplies two integers.
+ * @a: input number.
+ * @b: input number.
+ * Author - Nedu Robert
+ *
+ * Return: Returns the multiplication of the two inputs.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
