51aef52cfb103f5a97e1344ba790462aef090f24
Nedu Robert <nedumrobert@gmail.com>
2023-10-13 14:10:42 -0400

    Fizz-Buzz task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,31 @@
  * print FizzBuzz.
  * Author - Nedu Robert
  *
- * Return: void.
+ * Return: 0.
  */
 int main(void)
 {
-    int i;
+	int i;
 
 	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			if (i % 3 == 0 && i % 5 == 0)
-            {
-                printf("FizzBuzz ");
-            }
-            else if (i % 3 == 0)
-            {
-                printf("Fizz ");
-            }
-            else if (i % 5 == 0)
-            {
-                printf("Buzz ");
-            }
-            else
-            {
-                printf("%d ", i);
-            }
+			printf("FizzBuzz ");
 		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
 }
