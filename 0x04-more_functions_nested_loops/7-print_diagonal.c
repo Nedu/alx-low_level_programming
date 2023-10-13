bbe7c2857adb071e18c0740663e93424cae80cd0
Nedu Robert <nedumrobert@gmail.com>
2023-10-13 13:46:52 -0400

    print_diagonal task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_line - I feel like I am diagonally parked in a parallel universe
+ * Function that draws a diagonal line in the terminal.
+ * @n: input number
+ * Author - Nedu Robert
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+            for (j = 0; j < n; j++)
+            {
+                if (j == i)
+                    _putchar('\\');
+                if (j < i)
+                {
+                    _putchar(' ');
+                }
+            }
+	        _putchar('\n');
+		}
+	}
+    else
+    {
+        _putchar('\n');
+    }
+}
