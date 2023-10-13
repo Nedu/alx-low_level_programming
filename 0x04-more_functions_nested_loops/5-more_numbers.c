67b5575d5b5894137295bae3d9af74bf675d4e20
Nedu Robert <nedumrobert@gmail.com>
2023-10-13 13:03:34 -0400

    fixed betty issues

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,9 @@ void more_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
-        {
-            _putchar(j);
-        }
-        _putchar('\n');
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }
