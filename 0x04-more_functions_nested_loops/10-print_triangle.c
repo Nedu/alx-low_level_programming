d8d38a591bf18d8e6f631e5c324db922b864dee0
Nedu Robert <nedumrobert@gmail.com>
2023-10-13 14:59:18 -0400

    print_triangle task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Triangles
+ * Function that prints a triangle, followed by a new line.
+ * @size: input number for size of the triangle
+ * Author - Nedu Robert
+ *
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+            for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
