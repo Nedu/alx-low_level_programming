1e8c8f158d867a3ceab24c574cc454bfe8f94254
Nedu Robert <nedumrobert@gmail.com>
2023-10-12 23:02:32 -0400

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - I believe in numbers and signs
+ * Function that prints the numbers, from 0 to 9,
+ * followed by a new line except 2 and 4.
+ * Author - Nedu Robert
+ *
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
+}
