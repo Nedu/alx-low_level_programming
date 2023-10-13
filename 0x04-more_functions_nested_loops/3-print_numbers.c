d51846f37a79b81c3efef10c12ff70b6f35e5aeb
Nedu Robert <nedumrobert@gmail.com>
2023-10-12 23:00:22 -0400

    fixed betty issue

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mul - The numbers speak for themselves
+ * print_numbers - The numbers speak for themselves
  * Function that prints the numbers, from 0 to 9,
  * followed by a new line.
  * Author - Nedu Robert
  *
- * Return: Returns the multiplication of the two inputs.
+ * Return: void.
  */
 void print_numbers(void)
 {
