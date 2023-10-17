6765bf8ae8cf15ae8d5a0c26c22645f9bf155dac
Nedu Robert <nedumrobert@gmail.com>
2023-10-16 22:27:27 -0400

    fixed tab issue

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,12 +10,12 @@
  */
 int _strlen(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*s != 0)
-    {
-        s++;
-        i++;
-    }
-    return (i);
+	while (*s != 0)
+	{
+		s++;
+		i++;
+	}
+	return (i);
 }
