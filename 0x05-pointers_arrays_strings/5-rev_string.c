10e4b20a28cbf3dc4ed6a7f7557299665ead7bb8
Nedu Robert <nedumrobert@gmail.com>
2023-10-16 22:54:23 -0400

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - A good engineer thinks in reverse and asks himself
+ * about the stylistic consequences of the components and systems he proposes
+ * Function that reverses a string.
+ * @s: pointer to char s.
+ * Author - Nedu Robert
+ * Return: Returns void.
+ */
+void rev_string(char *s);
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
