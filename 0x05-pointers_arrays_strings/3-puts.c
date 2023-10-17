ae52c63af30eb891f4d5551bd2d2a4e44d8f93a1
Nedu Robert <nedumrobert@gmail.com>
2023-10-16 22:36:58 -0400

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - I do not fear computers. I fear the lack of them
+ * Function that prints a string, followed by a new line, to stdout.
+ * @str: pointer to char str.
+ * Author - Nedu Robert
+ * Return: Returns void.
+ */
+void _puts(char *str)
+{
+	for(; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
