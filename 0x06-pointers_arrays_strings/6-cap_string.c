9f85fb042b5f3340f428c4f186a910537fabcb45
Nedu Robert <nedumrobert@gmail.com>
2023-10-20 22:44:22 -0400

    fixed betty issues

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@
 char *cap_string(char *str)
 {
 	int i, j, count;
-	char separators[13] = {' ', '\t', '\n', ',', ';', '.','!', '?',
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
 	 '"', '(', ')', '{', '}'};
 	count = 0;
 
@@ -22,20 +22,20 @@ char *cap_string(char *str)
 
 	for(i = 0; i < count; i++)
 	{
-	   if(i == 0 && str[i] >= 'a' && str[i] <= 'z')
-	   {
-		str[i] = str[i] - 32;
-	   }
-	   for (j = 0; j < 13; j++)
-	   {
-		if(str[i] == separators[j])
+		if(i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i + 1] >= 'a' && str[i+1] <= 'z')
+			str[i] = str[i] - 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if(str[i] == separators[j])
 			{
-				str[i+1] = str[i+1] - 32;
+				if (str[i + 1] >= 'a' && str[i+1] <= 'z')
+				{
+					str[i+1] = str[i+1] - 32;
+				}
 			}
 		}
-	   }
 	}
 
 	return (str);
