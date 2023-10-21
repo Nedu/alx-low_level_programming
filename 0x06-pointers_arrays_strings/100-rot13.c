b7cf87b48f58ab49973519d90658ebef85d7676a
Nedu Robert <nedumrobert@gmail.com>
2023-10-20 22:59:00 -0400

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - rot13
+ * Function that encodes a string using rot13.
+ * @str: pointer to char str.
+ * Author - Nedu Robert
+ * Return: Returns encoded string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+	for (j = 0; a[j] != '\0'; j++)
+	{
+		if (str[i] == a[j])
+		{
+			str[i] = b[j];
+		}
+	}
+	}
+
+	return (str);
+}
