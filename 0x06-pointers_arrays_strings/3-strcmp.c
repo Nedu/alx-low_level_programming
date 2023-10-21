c693bacaeafb50478c17591871dd721f850c2f99
Nedu Robert <nedumrobert@gmail.com>
2023-10-20 22:03:51 -0400

    tasks 0 - 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - strcmp
+ * Function that compares two strings.
+ * @s1: pointer to char s1.
+ * @s2: pointer to char s2.
+ * Author - Nedu Robert
+ * Return: Returns an integer greater than, equal to, or less than 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while(*s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
