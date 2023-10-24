cf8b5cae30909e81f8ddcc4896c8830301787340
Nedu Robert <nedumrobert@gmail.com>
2023-10-24 15:45:40 -0400

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - memset
+ * Function that fills memory with a constant byte.
+ * @s: pointer to char memory area s.
+ * @b: input char b.
+ * @n: input unsigned int n, number of bytes.
+ * Author - Nedu Robert
+ * Return: Returns a pointer to memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+        s[i] = b;
+	}
+
+	return (s);
+}
