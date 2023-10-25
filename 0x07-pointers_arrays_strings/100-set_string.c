8d09dcf8dfa36b0865f2491ce679df061de206e0
Nedu Robert <nedumrobert@gmail.com>
2023-10-24 21:10:54 -0400

    task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Double pointer, double fun
+ * Function that sets the value of a pointer to a char.
+ * @s: double pointer to char s
+ * @to: oiinter to char to
+ * Author - Nedu Robert
+ * Return: Returns void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
