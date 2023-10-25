b2ac37207b99256a157003ffcc8e82f03b4be583
Nedu Robert <nedumrobert@gmail.com>
2023-10-24 20:49:18 -0400

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Chess is mental torture
+ * Function that prints the chessboard.
+ * @a: input array
+ * Author - Nedu Robert
+ * Return: Returns void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            _putchar(a[i][j]);
+        }
+        _putchar('\n');
+    }
+}
