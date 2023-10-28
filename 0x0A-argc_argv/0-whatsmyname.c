840ef681d5afa0d5266fee3bceafeae5ca6ee1a4
Nedu Robert <nedumrobert@gmail.com>
2023-10-27 22:37:20 -0400

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - strstr
+ * Program that prints its name, followed by a new line.
+ * @argc: int argc.
+ * @argv: char argv array.
+ * Author - Nedu Robert
+ * Return: Returns 0.
+ */
+int main(int argc, char *argv[])
+{
+    (void) argc;
+	printf("%s\n", argv[0]);
+
+    return (0);
+}
