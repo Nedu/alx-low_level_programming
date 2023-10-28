987495648c4d246cf2d1f2228e641263e7010736
Nedu Robert <nedumrobert@gmail.com>
2023-10-27 22:45:32 -0400

    tasks 1 and 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main function
+ * Program that prints all arguments it receives.
+ * @argc: int argc.
+ * @argv: char argv array.
+ * Author - Nedu Robert
+ * Return: Returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
