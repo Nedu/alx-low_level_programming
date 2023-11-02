e91b3f796b54ebde429aa55201bde8283c27cb92
Nedu Robert <nedumrobert@gmail.com>
2023-11-01 21:32:28 -0400

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,35 +15,33 @@
 int main(int argc, char *argv[])
 {
 	int i, input;
-    int min = 0;
-    int coins[] = { 25, 10, 5, 2, 1 };
+	int min = 0;
+	int coins[] = { 25, 10, 5, 2, 1 };
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-        return (1);
+		return (1);
 	}
 	else
 	{
-        input = atoi(argv[1]);
-        if (input < 0)
-        {
-            printf("0\n");
-        }
-        else
-        {
-            for (i = 0; i < 5; i++)
-            {
-                while (input >= coins[i])
-                {
-                    min++;
-                    input -= coins[i];
-                }
-            }
-
-        }
-		printf("%d\n", min);
+		input = atoi(argv[1]);
+		if (input < 0)
+		{
+			printf("0\n");
+		}
+		else
+		{
+			for (i = 0; i < 5; i++)
+			{
+				while (input >= coins[i])
+				{
+					min++;
+					input -= coins[i];
+				}
+			}
+			printf("%d\n", min);
+		}
 	}
-
 	return (0);
 }
