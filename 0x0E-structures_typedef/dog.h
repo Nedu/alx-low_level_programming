7436f1966c7c52cd27d3596f93ca0a7c8b249911
Nedu Robert <nedumrobert@gmail.com>
2023-11-06 22:09:42 -0500

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - Short description
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The ownwer of the dog
+ *
+ * Description: Dog's attributes
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
