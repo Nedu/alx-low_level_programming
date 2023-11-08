470a71d42e62214ba8cd2476306f5835d78d749e
Nedu Robert <nedumrobert@gmail.com>
2023-11-08 17:27:55 -0500

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * print_name - What's my name
+ * Function that prints a name.
+ * @name: The name to pass to the function to print
+ * @f: pointer to function
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
