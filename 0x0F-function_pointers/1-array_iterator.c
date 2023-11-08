b6f3a9038437e803db25928112deb9d2512c8a66
Nedu Robert <nedumrobert@gmail.com>
2023-11-08 17:54:55 -0500

    fixed betty issues

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,8 +14,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long i;
-    for (i = 0; i < size; i++)
-    {
-        action(array[i]);
-    }
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
