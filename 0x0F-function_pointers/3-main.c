4c5f6c5b392e5dd3475777481b5d9eacd6ef338f
Nedu Robert <nedumrobert@gmail.com>
2023-11-08 18:44:15 -0500

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * main - Sum of two inputs
+ * @argc: int argc.
+ * @argv: char argv array.
+ * 
+ * Author - Nedu Robert
+ * Return: Returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j,result;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		operator = get_op_func(argv[2]);
+		j = atoi(argv[3]);
+
+		if (!operator)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		result = operator(i, j);
+
+		printf("%d\n", result);
+		return (0);
+	}
+}
