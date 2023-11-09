7a5770f34e4cf44a17b6dc0a164197e7ecb3ab1a
Nedu Robert <nedumrobert@gmail.com>
2023-11-09 11:57:08 -0800

    fixed betty issues

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,12 +22,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
 
 	va_end(list);
 
-	return sum;
+	return (sum);
 }
