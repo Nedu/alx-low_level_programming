e755d33a013fcb818ee2577710fe0c1ea691bf19
Nedu Robert <nedumrobert@gmail.com>
2023-11-22 18:38:39 -0500

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - Free list
+ * Function that frees a listint_t list.
+ * @head: pointer to the list
+ *
+ * Author - Nedu Robert
+ * Return: Returns void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	while(head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
