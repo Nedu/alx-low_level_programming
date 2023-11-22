3785a2413b8372540817d55cb2e7485eb608c660
Nedu Robert <nedumrobert@gmail.com>
2023-11-22 17:52:33 -0500

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - Print list
+ * Function that prints all the elements of a listint_t list.
+ * @h: pointer to the list
+ *
+ * Author - Nedu Robert
+ * Return: Returns the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+    size_t length = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		length++;
+	}
+
+	return (length);
+}
