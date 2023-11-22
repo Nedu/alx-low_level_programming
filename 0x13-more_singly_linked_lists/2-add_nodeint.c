c39e4bc7fd1e83516ff991941258af6d5f2622dd
Nedu Robert <nedumrobert@gmail.com>
2023-11-22 18:20:19 -0500

    fixed betty issue

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp;
 
-	temp = malloc(sizeof (listint_t));
+	temp = malloc(sizeof(listint_t));
 	if (!temp)
 	{
 		return (NULL);
