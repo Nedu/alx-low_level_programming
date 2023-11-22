36ff2d26d1a7fbdec1d226460f971ea9890a13a2
Nedu Robert <nedumrobert@gmail.com>
2023-11-22 18:04:19 -0500

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - List length
+ * Function that returns the number of elements in a linked listint_t list.
+ * @h: pointer to the list
+ *
+ * Author - Nedu Robert
+ * Return: Returns the number of elements in a linked listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t numElements = 0;
+
+	while (h)
+	{
+		h = h->next;
+		numElements++;
+	}
+
+	return (numElements);
+}
