5a15c304062115ae653eb8aaa096f92ea8a6f8af
Nedu Robert <nedumrobert@gmail.com>
2023-11-22 18:18:07 -0500

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - List length
+ * Function that returns the number of elements in a linked listint_t list.
+ * @head: pointer to the list
+ * @n: pointer to the list
+ *
+ * Author - Nedu Robert
+ * Return: Returns the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof (listint_t));
+	if (!temp)
+	{
+		return (NULL);
+	}
+	temp->next = *head;
+	temp->n = n;
+	*head = temp;
+
+	return (temp);
+}
