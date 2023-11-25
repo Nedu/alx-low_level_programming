69598ed67cca2f834983ae3c7cef5478f10a12fe
Nedu Robert <nedumrobert@gmail.com>
2023-11-24 21:11:07 -0500

    fixed betty issues

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,19 +13,22 @@ void print_binary(unsigned long int n)
 	int j, binaryArr[32];
 	int i = 0;
 
-	if (n == 0) {
+	if (n == 0) 
+	{
 		printf("0");
 		return;
 	}
 	
 	
-	while (n > 0) {
+	while (n > 0) 
+	{
 		binaryArr[i] = n & 1;
 		n = n >> 1;
 		i++;
 	}
 	
-	for (j = i - 1; j >= 0; j--) {
+	for (j = i - 1; j >= 0; j--) 
+	{
 		printf("%d", binaryArr[j]);
 	}
 }
