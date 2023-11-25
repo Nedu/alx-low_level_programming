a2fe8dafab1cf147f68c4c4ab116dd2a00451fea
Nedu Robert <nedumrobert@gmail.com>
2023-11-24 21:03:00 -0500

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - 1
+ * Function that prints the binary representation of a number.
+ * @n: number to be converted
+ * Author - Nedu Robert
+ * Return: Returns void.
+ */
+void print_binary(unsigned long int n)
+{
+	int j, binaryArr[32];
+	int i = 0;
+
+	if (n == 0) {
+		printf("0");
+		return;
+	}
+	
+	
+	while (n > 0) {
+		binaryArr[i] = n & 1;
+		n = n >> 1;
+		i++;
+	}
+	
+	for (j = i - 1; j >= 0; j--) {
+		printf("%d", binaryArr[j]);
+	}
+}
