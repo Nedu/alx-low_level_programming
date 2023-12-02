eae9c5c83ea129fd467246c66e017bb6e522a8a3
Nedu Robert <nedumrobert@gmail.com>
2023-12-01 21:40:40 -0500

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Under the snow
+ * Function that creates a file.
+ * @filename: name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
+ * Author - Nedu Robert
+ * Return: Returns 1 on success, 
+ * -1 on failure (file can not be created, 
+ * file can not be written, write “fails”, filename is NULL)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int open_file, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	open_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (open_file == -1)
+		return (-1);
+
+	close(open_file);
+	return (1);
+}
